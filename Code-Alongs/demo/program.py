824ea7e3afe140f88299f134e11d26e8ad322104
Kevin Thulin <kevin.thulin@hotmail.com>
2023-09-08 09:03:52 +0200

    tasks 1

--- Code-Alongs/demo/program.py
+++ Code-Alongs/demo/program.py
@@ -1,1 +1,13 @@
-print("hello World")
+thornback_tortoise = 178
+tortoise = 108
+blood_crab = 184
+
+task_tort = 350 + 700 + 1700
+task_thorn_tort = 350 + 700 + 1700
+task_blood_crab = 350 + 700 + 1700
+
+task_1 = thornback_tortoise * task_thorn_tort
+task_2 = tortoise * task_tort
+task_3 = blood_crab * task_blood_crab
+
+print(task_1 + task_2 + task_3)
