ed7528d2a2d16785bd68b9b947cd7c5e28b227a4
Kevin Thulin <kevin.thulin@hotmail.com>
2023-09-22 13:52:22 +0200

    better readabillity in the code

--- Lab_and_Explorations/Lab_01.py
+++ Lab_and_Explorations/Lab_01.py
@@ -1,13 +1,24 @@
-import matplotlib.pyplot as plt
 
-# Import data from txt files
-with open("datapoints.txt", "r") as f:
-    data = f.readlines()[1:]
-    datapoints = [(float(point.split(",")[0]), float(point.split(",")[1]), int(point.split(",")[2])) for point in data]
 
-with open("testpoints.txt", "r") as f:
-    data = f.readlines()[1:]
-    testpoints = [(float(point.split(",")[0].split("(")[1]), float(point.split(",")[1].split(")")[0])) for point in data]
+def read_datapoints(datapoint): # Import data from txt files
+    with open(datapoint, "r") as f:
+        data = f.readlines()[1:]
+    return [process_data_points(point) for point in data ]
+    
+def process_data_points(point):   
+    x, y, label = point.split(",")
+    return float(x), float(y), int(label)
+
+def read_testpoints(testpoints):
+    with open(testpoints, "r") as f:
+        data = f.readlines()[1:]
+    return [process_test_point(point) for point in data]
+
+def process_test_point(point):
+    x_str, y_str = point.split(",")
+    x = float(x_str.split("(")[1])
+    y = float(y_str.split(")")[0])
+    return x, y
 
 # Function to calculate distance
 def distance(p1, p2):
@@ -22,6 +33,9 @@ def classify(point):
     pikachus = 10 - pichus
     return 0 if pichus > pikachus else 1
 
+testpoints = read_testpoints("testpoints.txt")
+datapoints = read_datapoints("datapoints.txt")
+
 # Classify the testpoints from the file
 results = [classify(testpoint) for testpoint in testpoints]
 
@@ -31,5 +45,5 @@ for testpoint, result in zip(testpoints, results):
     classification = "Pikachu" if result == 1 else "Pichu"
     output_strings.append(f"Sample with (width, height): {testpoint} classified as {classification}")
 
-output_strings
+print(output_strings)
 
