5755a04b54d7682332dc431d8f7d2c5af1f3e6cb
Kevin Thulin <kevin.thulin@hotmail.com>
2023-09-22 14:31:22 +0200

    added user_input to the code

--- Lab_and_Explorations/Lab_01.py
+++ Lab_and_Explorations/Lab_01.py
@@ -1,19 +1,36 @@
-
-
-def read_datapoints(datapoint): # Import data from txt files
+# Promt the user with widght and height.
+# Return the corrdiantes as a tuples of floats
+def input_from_user():
+    while True:
+        try:
+            height = float(input("Enter height: "))
+            weight = float(input("Enter weight: "))
+            if height <= 0 or weight <= 0:
+                print("Enter a positive number for both height and wight")
+                continue
+            return height, weight
+        except ValueError:
+            print("Enter a number!")
+        
+    
+# Read in the datapoints from a txt file and skip the header
+def read_datapoints(datapoint):
     with open(datapoint, "r") as f:
         data = f.readlines()[1:]
     return [process_data_points(point) for point in data ]
-    
+
+#  Split the in three and convret them in to floats or int
 def process_data_points(point):   
     x, y, label = point.split(",")
     return float(x), float(y), int(label)
 
+# Read in the testpoints from a txt file and skip header
 def read_testpoints(testpoints):
     with open(testpoints, "r") as f:
         data = f.readlines()[1:]
     return [process_test_point(point) for point in data]
 
+# split testoints into two and start with ( and ends with )
 def process_test_point(point):
     x_str, y_str = point.split(",")
     x = float(x_str.split("(")[1])
@@ -35,7 +52,8 @@ def classify(point):
 
 testpoints = read_testpoints("testpoints.txt")
 datapoints = read_datapoints("datapoints.txt")
-
+user_points = input_from_user()
+result = classify(user_points)
 # Classify the testpoints from the file
 results = [classify(testpoint) for testpoint in testpoints]
 
@@ -45,5 +63,8 @@ for testpoint, result in zip(testpoints, results):
     classification = "Pikachu" if result == 1 else "Pichu"
     output_strings.append(f"Sample with (width, height): {testpoint} classified as {classification}")
 
-print(output_strings)
 
+print("\n".join(output_strings))
+
+classification = "Pikachu" if result == 1 else "Pichu"
+print(f"Sample with (width, height): {user_points} and classified as {classification}")
