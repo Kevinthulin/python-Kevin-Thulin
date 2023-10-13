f1d8f2b1115b314a6310b0d5f7c806a33ef85393
Kevin Thulin <Kevin.thulin@hotmail.com>
2023-10-13 15:18:39 +0200

    classes

--- Code-Alongs/demo/program.py
+++ Code-Alongs/demo/program.py
@@ -1,3 +1,24 @@
-frukter = ["jordgubbe", "vindruva", "grape"]
-godfrukt = frukter[2]
-print(godfrukt[-1])
+import random
+
+
+
+
+def dice(throws):
+    outcomes = []
+    diced_rolled = {
+        1: 0,
+        2: 0,
+        3: 0,
+        4: 0,
+        5: 0,
+        6: 0
+    }
+    for _ in range(throws):
+        roll = random.randint(1, 6)
+        outcomes.append(roll)
+        diced_rolled[roll] += 1
+    return diced_rolled
+
+        
+
+print(dice(100000))
