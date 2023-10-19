86ea6a254da690bf241874e8ac16094de91691c2
Kevin Thulin <kevin.thulin@hotmail.com>
2023-10-19 19:50:24 +0200

    Changed cubes x,y,z

--- Lab3/Main.py
+++ Lab3/Main.py
@@ -2,12 +2,12 @@ from shapes2D import Circle, Rectangle
 from shapes3D import Sphere, Cube
 
 def main():
-    
+
     circle1 = Circle(2, 3, 5)
     rect1 = Rectangle(2, 1, 10, 5)
 
     sphere1 = Sphere(4, 4, 4, 5)
-    cube1 = Cube(1, 2, 3, 6.32)
+    cube1 = Cube(2, 2, 2, 6.32)
 
     print(f"Circle details: {circle1}")
     print(f"Rectangle details: {rect1}")
